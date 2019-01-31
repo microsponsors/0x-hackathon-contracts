b0da3059d74c310a35abf3d6eea480333863920d
Lauren Garcia <lauren.garcia@gmail.com>
2019-01-30 22:22:04 -0800

    match fn signatures between contracts

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -6,7 +6,7 @@ contract ERC721 {
     // Required methods
     function totalSupply() public view returns (uint256 total);
     function balanceOf(address _owner) public view returns (uint256 balance);
-    // function ownerOf(uint256 _tokenId) external view returns (address owner);
+    function ownerOf(uint256 _tokenId) public view returns (address owner);
     function approve(address _to, uint256 _tokenId) external;
     // function transfer(address _to, uint256 _tokenId) external;
     // function transferFrom(address _from, address _to, uint256 _tokenId) external;
